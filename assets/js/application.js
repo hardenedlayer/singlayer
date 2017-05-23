1b91f55d34580f25b7acab8b9694b7b6a705b572
Yonghwan SO <sio4@users.noreply.github.com>
2017-05-23 23:17:35 +0900

    applied dark theme and ui clean up

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,20 @@
 require('expose-loader?$!expose-loader?jQuery!jquery');
 require("bootstrap/dist/js/bootstrap.js");
 
+$(document).ready(function(){
+	$('[data-toggle="popover"]').popover();
+});
+
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function(){
+	window.setTimeout(function() {
+		$(".alert").alert('close');
+	}, 3000);
+});
+
 $(() => {
 
 });
