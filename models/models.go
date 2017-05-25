f321467d485560e1937d041d6904e4427037c49c
Yonghwan SO <sio4@users.noreply.github.com>
2017-05-25 18:43:49 +0900

    added logger and inspect function for debugging of models

--- models/models.go
+++ models/models.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 
 	"github.com/gobuffalo/envy"
 	"github.com/markbates/pop"
@@ -10,6 +12,7 @@ import (
 // DB is a connection to your database to be used
 // throughout your application.
 var DB *pop.Connection
+var Logger *log.Logger
 
 func init() {
 	var err error
@@ -19,4 +22,21 @@ func init() {
 		log.Fatal(err)
 	}
 	pop.Debug = env == "development"
+
+	Logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+// Additional functions for my debugging.
+//
+
+// inspect: to check data type and value.
+func inspect(desc string, data interface{}) {
+	Logger.Printf("{\"description\":\"%s\", \"datatype\":\"%T\", \"data\":%v}",
+		desc, data, toJSON(data))
+}
+
+// toJSON returns JSON formatted string with given data.
+func toJSON(data interface{}) string {
+	str, _ := json.Marshal(data)
+	return string(str)
 }
