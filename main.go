e3b6b15b05f3990df733c70e10974f2f49430db7
Yonghwan SO <sio4@users.noreply.github.com>
2017-06-08 22:20:52 +0900

    add tls support

--- main.go
+++ main.go
@@ -11,6 +11,14 @@ import (
 
 func main() {
 	port := envy.Get("PORT", "3000")
+	use_tls := envy.Get("USE_TLS", "no")
 	log.Printf("Starting singlayer on port %s\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), actions.App()))
+	if use_tls == "yes" {
+		tls_cert := envy.Get("TLS_CERT", "cert.pem")
+		tls_key := envy.Get("TLS_KEY", "key.pem")
+		log.Fatal(http.ListenAndServeTLS(fmt.Sprintf(":%s", port),
+			tls_cert, tls_key, actions.App()))
+	} else {
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), actions.App()))
+	}
 }
